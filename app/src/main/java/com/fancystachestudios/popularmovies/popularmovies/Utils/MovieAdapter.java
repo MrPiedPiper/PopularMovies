1503768e7e3f9f9fb3cf6f7b4b33f694ed6e9a51
MrPiedPiper <35707072+MrPiedPiper@users.noreply.github.com>
2018-05-21 15:59:45 -0700

    Load images

--- app/src/main/java/com/fancystachestudios/popularmovies/popularmovies/Utils/MovieAdapter.java
+++ app/src/main/java/com/fancystachestudios/popularmovies/popularmovies/Utils/MovieAdapter.java
@@ -1,15 +1,19 @@
 package com.fancystachestudios.popularmovies.popularmovies.Utils;
 
 import android.graphics.Movie;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.fancystachestudios.popularmovies.popularmovies.MovieAPI.MovieAPIManager;
 import com.fancystachestudios.popularmovies.popularmovies.MovieAPI.MovieObject;
 import com.fancystachestudios.popularmovies.popularmovies.R;
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,12 +24,17 @@ import java.util.List;
 
 public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> {
     private ArrayList<MovieObject> mDataset;
+    private MovieAPIManager movieAPIManager = new MovieAPIManager();
 
     public static class ViewHolder extends  RecyclerView.ViewHolder{
         public TextView mTextView;
+        public ConstraintLayout mLayout;
+        public ImageView mImageView;
         public ViewHolder (View v){
             super(v);
-            mTextView = v.findViewById(R.id.test_text);
+            mLayout = v.findViewById(R.id.item_root_layout);
+            mTextView = v.findViewById(R.id.item_list_number_text);
+            mImageView = v.findViewById(R.id.item_background_image);
         }
     }
 
@@ -44,12 +53,11 @@ public class MovieAdapter extends RecyclerView.Adapter<MovieAdapter.ViewHolder> 
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Log.d("mytest", "viewholder #:" + String.valueOf(position));
-        Log.d("mytest", String.valueOf(mDataset.size()));
-        Log.d("mytest", mDataset.get(position).getTitle());
-        Log.d("mytest", mDataset.get(position).getBackdropPath());
-        Log.d("mytest", mDataset.get(position).getPosterPath());
-        holder.mTextView.setText(mDataset.get(position).getTitle());
+
+        String posterPath = movieAPIManager.getPosterPath(mDataset.get(position));
+        Picasso.get().load(posterPath).into(holder.mImageView);
+        Log.d("mytest", posterPath);
+        holder.mTextView.setText(String.valueOf(position));
     }
 
     @Override
