3ab9b7dda6dfd886f85393bcb69eb2af2ef972c3
MrPiedPiper <35707072+MrPiedPiper@users.noreply.github.com>
2018-05-24 11:26:17 -0700

    clean project for submission

--- app/src/main/java/com/fancystachestudios/popularmovies/popularmovies/MainActivity.java
+++ app/src/main/java/com/fancystachestudios/popularmovies/popularmovies/MainActivity.java
@@ -39,7 +39,7 @@ public class MainActivity extends AppCompatActivity
             MovieAdapter.MovieClickListener,
             LoaderManager.LoaderCallbacks<ArrayList<MovieObject>>{
 
-    //Prepare RecyclerView variables
+    ///Prepare RecyclerView variables
     @BindView(R.id.mainRecyclerView) RecyclerView mRecyclerView;
     private MovieAdapter mAdapter;
     MovieAdapter.MovieClickListener movieClickListener;
