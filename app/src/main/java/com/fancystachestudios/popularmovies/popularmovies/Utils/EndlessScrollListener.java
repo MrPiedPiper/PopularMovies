9b1e1ebf4bc9de6255697ec8e1939e0758bd58b3
MrPiedPiper <35707072+MrPiedPiper@users.noreply.github.com>
2018-05-24 10:48:09 -0700

    Comment the infinite scroll

--- app/src/main/java/com/fancystachestudios/popularmovies/popularmovies/Utils/EndlessScrollListener.java
+++ app/src/main/java/com/fancystachestudios/popularmovies/popularmovies/Utils/EndlessScrollListener.java
@@ -7,29 +7,38 @@ import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.widget.AbsListView;
 
 /**
- * Code from the "codepath" github repository. I don't have time to add comments at the time of writing, but you can find plenty of comments over at the links below. (Written by original author)
- * https://github.com/codepath/android_guides/wiki/Endless-Scrolling-with-AdapterViews-and-RecyclerView
- * https://gist.github.com/nesquena/d09dc68ff07e845cc622
+ * Code from the "codepath" github repository.
+ * Instructions I followed: https://github.com/codepath/android_guides/wiki/Endless-Scrolling-with-AdapterViews-and-RecyclerView
+ * File I copied: https://gist.github.com/nesquena/d09dc68ff07e845cc622
  */
 
 public abstract class EndlessScrollListener extends android.support.v7.widget.RecyclerView.OnScrollListener{
+    //Minimum amount of items below current scroll before loading more
     private int visibleThreshold = 2;
+    //Current offset
     private int currentPage = 0;
+    //Total count of items from the last load
     private int previousTotalItemCount = 0;
+    //True if we're waiting for the next dataset to load
     private boolean loading = true;
+    //Starting page index
     private int startingPageIndex = 0;
 
-    RecyclerView.LayoutManager mLayoutManager;
+    //The current LayoutManager
+    private RecyclerView.LayoutManager mLayoutManager;
 
+    //Constructor for LinearLayoutManager
     public EndlessScrollListener(LinearLayoutManager layoutManager){
         this.mLayoutManager = layoutManager;
     }
 
+    //Constructor for GridLayoutManager
     public EndlessScrollListener(GridLayoutManager layoutManager){
         this.mLayoutManager = layoutManager;
         this.visibleThreshold = visibleThreshold * layoutManager.getSpanCount();
     }
 
+    //Gets the last visible item
     public int getLastVisibleItem(int[] lastVisibleItemPosition){
         int maxSize = 0;
         for(int i=0; i<lastVisibleItemPosition.length; i++){
@@ -44,18 +53,28 @@ public abstract class EndlessScrollListener extends android.support.v7.widget.Re
 
     @Override
     public void onScrolled(android.support.v7.widget.RecyclerView recyclerView, int horizontalScrollAmount, int verticalScrollAmount) {
+        //Last visible item position defaults to 0
         int lastVisibleItemPosition = 0;
+        //Gets the total item count
         int totalItemCount = mLayoutManager.getItemCount();
 
         if(mLayoutManager instanceof StaggeredGridLayoutManager){
+            //If the LayoutManager is a StaggeredGridLayoutManager,
+            //Get the last visible item positions,
             int[] lastVisibleItemPositions = ((StaggeredGridLayoutManager) mLayoutManager).findLastVisibleItemPositions(null);
+            //And set the lastVisibleitemPosition to the last visible item
             lastVisibleItemPosition = getLastVisibleItem(lastVisibleItemPositions);
         }else if(mLayoutManager instanceof  GridLayoutManager){
+            //If the LayoutManager is a GridLayoutManager,
+            //Set the lastVisibleItemPosition to the last visible item
             lastVisibleItemPosition = ((GridLayoutManager)mLayoutManager).findLastVisibleItemPosition();
         }else if(mLayoutManager instanceof LinearLayoutManager){
+            //If the LayoutManager is a LinearLayoutManager,
+            //Set the lastVisibleItemPosition to the last visible item
             lastVisibleItemPosition = ((LinearLayoutManager)mLayoutManager).findLastVisibleItemPosition();
         }
 
+        //If the total item count is less than before, something must be wrong.
         if(totalItemCount < previousTotalItemCount) {
             this.currentPage = this.startingPageIndex;
             this.previousTotalItemCount = totalItemCount;
@@ -63,23 +82,32 @@ public abstract class EndlessScrollListener extends android.support.v7.widget.Re
                 this.loading = true;
             }
         }
+        //If loading and the totalItemCount is larger than the previousTotalItemCount, the loading must be done.
         if(loading && (totalItemCount > previousTotalItemCount)){
+            //Set the loading to false
             loading = false;
+            //Set the previousItemCount to the totalItemCount
             previousTotalItemCount = totalItemCount;
         }
+        //If not loading and we've scrolled down far enough (Based on the visibleThreshhold variable)
         if(!loading && (lastVisibleItemPosition + visibleThreshold) > totalItemCount){
+            //Increment the currentPage variable
             currentPage++;
-            onLoadMore(currentPage + 1, totalItemCount);
+            //Call onLoadMore with the currentPage and the totalItemCount
+            onLoadMore(currentPage, totalItemCount);
+            //And set loading to true
             loading = true;
         }
     }
 
+    //Function resets the state of the scroll
     public void resetState(){
         this.currentPage = this.startingPageIndex;
         this.previousTotalItemCount = 0;
         this.loading = true;
     }
 
+    //Function is added when the class is re-created in another class -If that makes sense-
     public abstract boolean onLoadMore(int page, int totalitemsCount);
 
 }
